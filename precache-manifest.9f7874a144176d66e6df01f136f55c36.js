de3a8dde4adba3e8b1d138126543597494d592b4
SzubaKrzysztof <szuba.christopher@gmail.com>
2020-07-22 22:55:45 +0200

    Updates

---
+++ precache-manifest.9f7874a144176d66e6df01f136f55c36.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa44365e4dd25df019cc055de034e3ce",
+    "url": "/deploy/index.html"
+  },
+  {
+    "revision": "0881deeda793e4b0886e",
+    "url": "/deploy/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "5aae0ebeaae19c5e7929",
+    "url": "/deploy/static/css/main.f81ecbcd.chunk.css"
+  },
+  {
+    "revision": "0881deeda793e4b0886e",
+    "url": "/deploy/static/js/2.002bdfc6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/deploy/static/js/2.002bdfc6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5aae0ebeaae19c5e7929",
+    "url": "/deploy/static/js/main.dbbd47bd.chunk.js"
+  },
+  {
+    "revision": "2877f3a407a81b5bcf24",
+    "url": "/deploy/static/js/runtime-main.b97e59d6.js"
+  }
+]);
